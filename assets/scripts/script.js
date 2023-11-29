69d7cf4390bf1579450755687164a78d7b4baddd
Hening Pereira <125718100+heningdev@users.noreply.github.com>
2023-11-29 18:08:33 -0300

    Upload pagina web  Clinica veterinária

---
+++ assets/scripts/script.js
@@ -0,0 +1,31 @@
+
+// Eventos  
+
+// Esconder/Exibir F.A.Q
+var elementosDuvida = document.querySelectorAll('.duvida');
+
+elementosDuvida.forEach(function (duvida) {
+    var icon = duvida.querySelector('.fa-caret-down');
+
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle('ativa');
+        
+        // Verifique a classe atual do ícone e alterne entre para cima e para baixo
+        if (icon.classList.contains('fa-caret-down')) {
+            icon.classList.remove('fa-caret-down');
+            icon.classList.add('fa-caret-up');
+        } else {
+            icon.classList.remove('fa-caret-up');
+            icon.classList.add('fa-caret-down');
+        }
+    });
+});
+
+
+
+// Voltar ao topo
+window.scroll({
+    top:0,
+    behavior:'smooth'
+}
+);
